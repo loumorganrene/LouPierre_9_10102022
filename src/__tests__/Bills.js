9cfca3153c9792c6a9005c0ef96189c27557d3ab
Lou René <75613368+loumorganrene@users.noreply.github.com>
2022-10-20 19:07:37 +0200

    Issue #3: [Bug Hunt]Bills - Modale should display proof image

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,13 +2,17 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import {fireEvent,screen, waitFor} from "@testing-library/dom"
+import userEvent from '@testing-library/user-event'
 import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
+import Action from "../views/Actions.js"
+import Bills from "../containers/Bills.js";
+import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
-
+import mockStore from "../__mocks__/store"
+import { bills } from "../fixtures/bills"
 import router from "../app/Router.js";
+
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -34,6 +38,34 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+  })
+})
+
+describe('Given I am connected as Employee and I am on Dashboard page and I clicked on a bill', () => {
+  describe('When I click on the icon eye', () => {
+    test('A modal should open', () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      document.body.innerHTML = BillsUI(bills[0])
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      const store = null
+      const billspage = new Bills({
+        document, onNavigate, store, localStorage: window.localStorage
+      })
+
+      const handleClickIconEye = jest.fn(billspage.handleClickIconEye)
+      const eye = screen.getByTestId('icon-eye')
+      eye.addEventListener('click', handleClickIconEye)
+      userEvent.click(eye)
+      expect(handleClickIconEye).toHaveBeenCalled()
+
+      const modale = screen.getByTestId('modaleFile')
+      expect(modale).toBeTruthy()
     })
   })
 })
