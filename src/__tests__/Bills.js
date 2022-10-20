808ce8d17a659eda9f2450815109b77629eed05f
Lou René <75613368+loumorganrene@users.noreply.github.com>
2022-10-21 00:11:45 +0200

    Issue#3: [Bug Hunt]Bills - Modale displaying on eye icon click test DONE

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,70 +2,81 @@
  * @jest-environment jsdom
  */
 
-import {fireEvent,screen, waitFor} from "@testing-library/dom"
+import "@testing-library/jest-dom";
+import { screen, waitFor } from "@testing-library/dom"
 import userEvent from '@testing-library/user-event'
-import BillsUI from "../views/BillsUI.js"
-import Action from "../views/Actions.js"
 import Bills from "../containers/Bills.js";
-import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+import BillsUI from "../views/BillsUI.js"
 import mockStore from "../__mocks__/store"
 import { bills } from "../fixtures/bills"
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
 import router from "../app/Router.js";
+import { Modal } from 'bootstrap';
 
+jest.mock("../app/store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
+      window.localStorage.setItem(
+        'user', 
+        JSON.stringify({
+          type: 'Employee'
+        })
+      )
+
       const root = document.createElement("div")
       root.setAttribute("id", "root")
       document.body.append(root)
       router()
       window.onNavigate(ROUTES_PATH.Bills)
+
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
 
+      expect(windowIcon).toHaveClass("active-icon")
     })
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
+
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
+
       expect(dates).toEqual(datesSorted)
     })
   })
-})
 
-describe('Given I am connected as Employee and I am on Dashboard page and I clicked on a bill', () => {
-  describe('When I click on the icon eye', () => {
-    test('A modal should open', () => {
+  describe('When I am on Bills Page and I click on a bill eye icon', () => {
+    test('Then a modal should open', async () => {
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      document.body.innerHTML = BillsUI(bills[0])
+      window.localStorage.setItem(
+        'user', 
+        JSON.stringify({
+          type: 'Employee'
+        })
+      )
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname })
       }
+      document.body.innerHTML = BillsUI({ data: bills })
+
       const store = null
-      const billspage = new Bills({
+      const billsPage = new Bills({
         document, onNavigate, store, localStorage: window.localStorage
       })
 
-      const handleClickIconEye = jest.fn(billspage.handleClickIconEye)
-      const eye = screen.getByTestId('icon-eye')
-      eye.addEventListener('click', handleClickIconEye)
-      userEvent.click(eye)
+      const iconEye = screen.getAllByTestId('icon-eye')[0]
+      const handleClickIconEye = jest.fn(billsPage.handleClickIconEye)
+      iconEye.addEventListener('click', handleClickIconEye(iconEye))
+      userEvent.click(iconEye)
       expect(handleClickIconEye).toHaveBeenCalled()
 
       const modale = screen.getByTestId('modaleFile')
-      expect(modale).toBeTruthy()
+      expect(modale).toBeVisible()
     })
   })
 })
