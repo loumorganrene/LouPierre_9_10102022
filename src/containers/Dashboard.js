56647dc9df777d4df53fe5a36a8b968f45f768d7
Lou René <75613368+loumorganrene@users.noreply.github.com>
2022-11-10 15:58:15 +0100

    Issue#4: [Bug hunt]Dashboard, Admin can select bills from different collapsible list = DONE

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -88,7 +88,7 @@ export default class {
   handleEditTicket(e, bill, bills) {
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    if (this.counter % 2 === 0) {
+    if (this.counter % 1 === 0) { //divisible par 1
       bills.forEach(b => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
       })
@@ -133,7 +133,7 @@ export default class {
   handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
-    if (this.counter % 2 === 0) {
+    if (this.counter % 2 === 0) { //divisible par 2
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
       $(`#status-bills-container${this.index}`)
         .html(cards(filteredBills(bills, getStatus(this.index))))
